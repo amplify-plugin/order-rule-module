ca705d1b5bd390d02b64cb66f1ae199d8301a04f
Hafijul Islam <hafijul.islam@easyask.com>
2024-12-30 01:00:19 +0600

    Update web.php

--- Routes/web.php
+++ Routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Amplify\Frontend\Http\Controllers\DynamicPageLoadController;
 use Amplify\System\OrderRule\Http\Controllers\CustomerOrderRuleController;
 use Amplify\System\OrderRule\Http\Controllers\OrderApprovalController;
 use Amplify\System\OrderRule\Http\Controllers\OrderAwaitingApprovalController;
@@ -22,20 +22,22 @@ Route::namespace('Amplify\System\OrderRule\Http\Controllers')->group(function ()
     });
 });
 
-Route::name('frontend.')->middleware(['web', 'customers'])->group(function () {
-    Route::resource('order-rules', OrderRuleController::class);
-    Route::resource('order-awaiting-approvals', OrderAwaitingApprovalController::class);
-    Route::get('order/{order_id}/checkout', [OrderAwaitingApprovalController::class, 'confirm'])->name('order.checkout');
-    Route::post('order/{order_id}/confirm', [OrderAwaitingApprovalController::class, 'action'])->name('order.action');
-    Route::resource('order-approvals', OrderApprovalController::class);
-    Route::resource('order-rejects', OrderRejectController::class);
-    Route::post('cart/order-rules-check', [OrderRuleController::class, 'cartOrderRuleCheck']);
+Route::name('frontend.')->group(function () {
+
+    Route::middleware(['web', 'customers'])->group(function () {
+        Route::resource('order-rules', OrderRuleController::class);
+        Route::resource('order-awaiting-approvals', OrderAwaitingApprovalController::class);
+        Route::get('order/{order_id}/checkout', [OrderAwaitingApprovalController::class, 'confirm'])->name('order.checkout');
+        Route::post('order/{order_id}/confirm', [OrderAwaitingApprovalController::class, 'action'])->name('order.action');
+        Route::resource('order-approvals', OrderApprovalController::class);
+        Route::resource('order-rejects', OrderRejectController::class);
+        Route::post('cart/order-rules-check', [OrderRuleController::class, 'cartOrderRuleCheck']);
+    });
+
+    Route::post('/fetch/order-rule', [CustomerOrderRuleController::class, 'fetchAvailableRule'])->name('fetch.order-rule');
+    Route::post('/fetch/rule', [CustomerOrderRuleController::class, 'fetchRuleField'])->name('fetch.rule-field');
+    Route::get('/fetch/contacts/{customer_id}', [CustomerOrderRuleController::class, 'fetchContactsByCustomer'])->name('fetch.contacts');
+    Route::delete('/order-rule/{rule}', [CustomerOrderRuleController::class, 'destroy'])->name('order-rule.destroy');
+    Route::get('/order-rule', [DynamicPageLoadController::class, 'index'])->name('order-rule');
+    Route::match(['post', 'put'], '/order-rule', [CustomerOrderRuleController::class, 'creataOrUpdate'])->name('save-order-rule');
 });
-
-Route::post('/fetch/order-rule', [CustomerOrderRuleController::class, 'fetchAvailableRule'])->name('frontend.fetch.order-rule');
-Route::post('/fetch/rule', [CustomerOrderRuleController::class, 'fetchRuleField'])->name('frontend.fetch.rule-field');
-Route::get('/fetch/contacts/{customer_id}', [CustomerOrderRuleController::class, 'fetchContactsByCustomer'])->name('frontend.fetch.contacts');
-
-Route::delete('/order-rule/{rule}', [CustomerOrderRuleController::class, 'destroy'])->name('order-rule.destroy');
-Route::get('/order-rule', [DynamicPageLoadController::class, 'orderRuleList'])->name('frontend.order-rule');
-Route::match(['post', 'put'], '/order-rule', [CustomerOrderRuleController::class, 'creataOrUpdate'])->name('frontend.save-order-rule');
